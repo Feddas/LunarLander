9f126f1f4867efa8b5979544fbccf4691b6fc1f6
Feddas <feddas00@gmail.com>
2013-03-12 15:45:00 -0700

    NGUI replaces 2 large ship navigation buttons.

--- Assets/Scripts/PlayerTouch.cs
+++ Assets/Scripts/PlayerTouch.cs
@@ -4,60 +4,40 @@ using System.Collections;
 public class PlayerTouch : MonoBehaviour {	
     //file:///C:/Apps/Unity/Editor/Data/Documentation/Documentation/Components/gui-Layout.html
     /// <summary> default to the bottom third of the screen </summary>
-    public Rect button = new Rect(0, 0, 100, 100);
     public PlayerMoveEnum choice = PlayerMoveEnum.Undetermined;
 	
     private void OnGUI()
     {
-        GUI.skin.button.fontSize = (Screen.height+Screen.width)/40;
-        if (button == new Rect(0, 0, 100, 100))
-            button = createButton(choice);
+    }
+	
+	bool isPressed;
+	void OnPress(bool isDown)
+	{
+		isPressed = isDown;
 		
-        if (GUI.RepeatButton(button, choice.ToString()))
+		if (isPressed == false)
+			count = 0;
+	}
+	
+	int count = 0;
+	void Update()
+	{
+		if (isPressed
+		    && Globals.BottomThruster != null
+			&& Globals.LeftThruster != null
+			&& Globals.RightThruster != null
+			&& Globals.PlayerShip != null)
 		{
-			if (Globals.BottomThruster != null
-				&& Globals.LeftThruster != null
-				&& Globals.RightThruster != null
-				&& Globals.PlayerShip != null)
+			Thrusters thrusters = new Thrusters()
 			{
-				Thrusters thrusters = new Thrusters()
-				{
-					ThrusterBottom = Globals.BottomThruster,
-					ThrusterLeft = Globals.LeftThruster,
-					ThrusterRight = Globals.RightThruster,
-				};
-				Globals.PlayerShip.AddForce(thrusters.DoThrust(choice));
-	            print(choice.ToString() + " fired");
-			}
+				ThrusterBottom = Globals.BottomThruster,
+				ThrusterLeft = Globals.LeftThruster,
+				ThrusterRight = Globals.RightThruster,
+			};
+			Globals.PlayerShip.AddForce(thrusters.DoThrust(choice));
+            print(choice.ToString() + " fire #" + count++);
 		}
-    }
-
-    private Rect createButton(PlayerMoveEnum choice)
-    {
-        float margin = 1;
-        float left;
-        float top = margin;
-        float width = Screen.width / 2 - (2 * margin);
-        float height = Screen.height - (2 * margin);
-        switch (choice)
-        {
-			//2 buttons, one on the left the other on the right
-            case PlayerMoveEnum.LeftBottomThruster:
-                left = 0 + margin;
-                return new Rect(left, top, width, height);
-            case PlayerMoveEnum.RightBottomThruster:
-                left = Screen.width / 2 + margin;
-                return new Rect(left, top, width, height);
-			
-			//All other types of movement are not handled
-            case PlayerMoveEnum.LeftThruster:
-            case PlayerMoveEnum.RightThruster:
-            case PlayerMoveEnum.BottomThruster:
-            case PlayerMoveEnum.Undetermined:
-            default:
-                return button;
-        }
-    }
+	}
 }
 
 public enum PlayerMoveEnum
--- README.md
+++ README.md
@@ -1,7 +1,19 @@
 LunarLander
 ===========
 
+What is it?
+-----------
 Unity3D implementation of the game Lunar Lander. Roughly based off the tutorial provided by CgCookie: http://cgcookie.com/unity/2012/02/24/game-planning-lunar-lander-01/
+
+Requirements
+------------
+Built using the free version of Unity 3D v3.5.7, which can be downloaded at http://unity3d.com/unity/download/archive
+Unity 3D Assets used:
+- educational/instructional version of NGUI found at 
+
+How to play
+-----------
+Use the arrow keys or the 2 large on screen buttons to navigate the craft from one landing pad to the next, turning the pads green as you go. You need to land the craft gently on the landing pads or it will explode.
 
 TODO
 ----
