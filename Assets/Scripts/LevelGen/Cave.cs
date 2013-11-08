8f65542fa0188941f13e19a35306dbc44b11c1ba
Shawn <feddas00@gmail.com>
2013-11-08 03:20:12 -0800

    create basic procedural cave walls

--- Assets/Scripts/LevelGen/Cave.cs
+++ Assets/Scripts/LevelGen/Cave.cs
@@ -29,10 +29,11 @@ public class Cave : MonoBehaviour
 	
 	private void createLandingPads()
 	{
+		//there will be a nullref if the landingPad is generated ontop of the players ship
 		var pad1 = this.helper.PositionNew(this.LandingPad, 
-			Random.Range(-10, 0), Random.Range(-10, -2));
+			Random.Range(-10, -4), Random.Range(-10, 4));
 		var pad2 = this.helper.PositionNew(this.LandingPad, 
-			Random.Range(0, 10), Random.Range(-10, -2));
+			Random.Range(4, 10), Random.Range(-10, 4));
 		
 		landingPadLocations.Add(helper.Vector3to2(pad1.transform.position));
 		landingPadLocations.Add(helper.Vector3to2(pad2.transform.position));
@@ -53,27 +54,38 @@ public class Cave : MonoBehaviour
 	private Mesh caveMesh()
 	{
 		MeshBuilder meshBuilder = new MeshBuilder();
+		int pads = landingPadLocations.Count;
+		float degreesPerPad = 360/pads;
+		IList<Vector2> wallCoords;//new List<Vector2>();
 		
-		helper.BuildWall(meshBuilder, new List<Vector2>() {
-			new Vector2(-10,-12),
-			new Vector2(-12,-6),
-			new Vector2(-10,0),
-			new Vector2(-12,6),
-			new Vector2(-2,12)
-		});
+		if (pads == 2)
+		{
+			Vector2 normal = 2*landingPadLocations[0] - landingPadLocations[1];
+			wallCoords = wallForPad(landingPadLocations[0], normal, degreesPerPad);
+			normal = 2*landingPadLocations[1] - landingPadLocations[0];
+			foreach (var coord in wallForPad(landingPadLocations[1], normal, degreesPerPad))
+			{
+				wallCoords.Add(coord);
+			}
+			//wallForPad(new Vector2(0,0), new Vector2(1,0), 180);
+			
+			//close off the cave
+			wallCoords.Add(wallCoords[0]);
+		}
+		else
+		{
+			Debug.Log("Unsupported number of landing pads");
+			return null;
+		}
 		
-		//TODO: replace code below with buildWall
-//		//left side
-//		buildQuad(meshBuilder,
-//			offset: new Vector3(-12, -6, -4),
-//			widthDir: new Vector3(0, 0, 8),
-//			lengthDir: new Vector3(0, 12, 0));
-//		
-//		//bottom
-//		buildQuad(meshBuilder,
-//			offset: new Vector3(5, -15, -4),
-//			widthDir: new Vector3(0, 0, 8),
-//			lengthDir: new Vector3(-10, 0, 0));
+		helper.BuildWall(meshBuilder, wallCoords);
+//		new List<Vector2>() {
+//			new Vector2(-10,-12),
+//			new Vector2(-12,-6),
+//			new Vector2(-10,0),
+//			new Vector2(-12,6),
+//			new Vector2(-2,12)
+//		});
 		
 		Mesh mesh = meshBuilder.CreateMesh();
 		mesh.RecalculateNormals();
@@ -81,31 +93,32 @@ public class Cave : MonoBehaviour
 		return mesh;
 	}
 	
-//	private void buildQuad(MeshBuilder meshBuilder,
-//		Vector3 offset,
-//		Vector3 widthDir,
-//		Vector3 lengthDir)
-//	{
-//		Vector3 normal = Vector3.Cross(lengthDir, widthDir).normalized;
-//		
-//		meshBuilder.Vertices.Add(offset);
-//		meshBuilder.UVs.Add(new Vector2(0.0f, 0.0f));
-//		meshBuilder.Normals.Add(normal);
-//		
-//		meshBuilder.Vertices.Add(offset + lengthDir);
-//		meshBuilder.UVs.Add(new Vector2(0.0f, 1.0f));
-//		meshBuilder.Normals.Add(normal);
-//		
-//		meshBuilder.Vertices.Add(offset + lengthDir + widthDir);
-//		meshBuilder.UVs.Add(new Vector2(1.0f, 1.0f));
-//		meshBuilder.Normals.Add(normal);
-//		
-//		meshBuilder.Vertices.Add(offset + widthDir);
-//		meshBuilder.UVs.Add(new Vector2(1.0f, 0.0f));
-//		meshBuilder.Normals.Add(normal);
-//		
-//		int baseIndex = meshBuilder.Vertices.Count - 4;
-//		meshBuilder.AddTriangle(baseIndex, baseIndex + 1, baseIndex + 2);
-//		meshBuilder.AddTriangle(baseIndex, baseIndex + 2, baseIndex + 3);
-//	}
+	private IList<Vector2> wallForPad(Vector2 padLocation, Vector2 padNormal, float degreeOfWall)
+	{
+		IList<Vector2> wall = new List<Vector2>();
+		degreeOfWall /= 2; // focus on halves, below the padNormal and above it.
+		
+		float increment = -1 * degreeOfWall;
+		Vector2 newPoint;
+		while (increment <= degreeOfWall)
+		{
+			newPoint = rotatePadFromNormal(padLocation, padNormal, increment);
+			wall.Add(newPoint);
+			increment += 30;
+		}
+		return wall;
+	}
+	
+	private Vector2 rotatePadFromNormal(Vector2 padLocation, Vector2 padNormal, float rotation)
+	{
+		rotation *= Mathf.Deg2Rad;
+		padNormal *= Random.Range(.9f, 1.1f); //add random variation to the magnitude to give the cave wall a more jagged look
+		
+		Vector2 newPoint;
+		//how to rotate: x_new = ( (x - cx) * cos(theta) + (y - cy) * sin(theta)) + cx http://stackoverflow.com/questions/14842090/rotate-line-around-center-point-given-two-vertices
+		newPoint.x = ( (padNormal.x - padLocation.x) * Mathf.Cos(rotation) + (padNormal.y - padLocation.y) * Mathf.Sin(rotation)) + padLocation.x;
+		newPoint.y = (-1 * (padNormal.x - padLocation.x) * Mathf.Sin(rotation) + (padNormal.y - padLocation.y) * Mathf.Cos(rotation)) + padLocation.y;
+				
+		return newPoint;
+	}
 }
