e9df4fd370cc53a35cd1ecb3d3d751b4c0acacb6
Shawn <feddas00@gmail.com>
2013-11-11 18:44:34 -0800

    WallBuild reuses pre-existing vertices

--- Assets/Scripts/LevelGen/GenHelper.cs
+++ Assets/Scripts/LevelGen/GenHelper.cs
@@ -16,6 +16,11 @@ public class GenHelper
 	}
 	
 	/// <summary>
+	/// When this is -1 the previous index was new, not pre-existing
+	/// </summary>
+	int existingVertexIndex = -1;
+	
+	/// <summary>
 	/// builds a wall with the normal pointing inwards if vertices are given clockwise
 	/// based off terrian of: http://jayelinda.com/wp/modelling-by-numbers-part-1a/
 	/// </summary>
@@ -28,23 +33,40 @@ public class GenHelper
 		int segments = vertices.Count - 1;
 		for (int index = 0; index < vertices.Count; index++)
 		{
+			// Add vertex
 			vector2 = vertices[index];
-			meshBuilder.Vertices.Add(Vector2to3(vector2, -4));
-			meshBuilder.Vertices.Add(Vector2to3(vector2, 4));
+			bool isDistinctVertex = (vertices.IndexOf(vector2) == index);
+			if (isDistinctVertex) // add new vertex
+			{
+				meshBuilder.Vertices.Add(Vector2to3(vector2, -4));
+				meshBuilder.Vertices.Add(Vector2to3(vector2, 4));
+			}
+			else // make use of existing vertex.
+			{
+				if (existingVertexIndex != -1)
+					Debug.Log("BuildWall is reusing too many vertices");
+				existingVertexIndex = vertices.IndexOf(vector2)
+					* 2 // because there are 2 vertices added per vector2 (z of 4 & -4)
+						+ 1; // use the z of 4 vertex
+			}
 			
+			//init UV map with values for index == 0
 			Vector2 uvFront = new Vector2(0,0),
 				uvBack = new Vector2(0,1);
 			
+			//connect with previous vertice
 			if (index > 0)
 			{
 				uvFront.x = uvBack.x = (1.0f / segments) * index;
 				
-				int baseIndex = meshBuilder.Vertices.Count - 1;
+				int[] indices = quadrilateralIndices(isDistinctVertex, meshBuilder.Vertices.Count);
+				if (indices == null)
+					continue; //skip adding triangles for this vertex iteration
 				
-				int index0 = baseIndex;
-				int index1 = baseIndex - 1;
-				int index2 = baseIndex - 2;
-				int index3 = baseIndex - 3;
+				int index0 = indices[0],
+					index1 = indices[1],
+					index2 = indices[2],
+					index3 = indices[3];
 				
 				meshBuilder.AddTriangle(index0, index2, index1);
 				meshBuilder.AddTriangle(index2, index3, index1);
@@ -65,5 +87,48 @@ public class GenHelper
 	public Vector2 Vector3to2(Vector3 vector)
 	{
 		return new Vector2(vector.x, vector.y);
+	}
+	
+	private int[] quadrilateralIndices(bool isNewVertex, int vertexCount)
+	{
+		int[] indices = new int[4];
+		int baseIndex = vertexCount - 1;
+		
+		// handle new vertex where the previous vertex was also new
+		if (isNewVertex //can use Vertices.count
+			&& existingVertexIndex == -1) //previous vertex was not preexisting
+		{
+			indices[0] = baseIndex;
+			indices[1] = baseIndex - 1;
+			indices[2] = baseIndex - 2;
+			indices[3] = baseIndex - 3;
+		}
+		
+		// handle new vertex where the previous vertex was old
+		else if (isNewVertex
+			&& existingVertexIndex != -1)
+		{
+			indices[0] = baseIndex;
+			indices[1] = baseIndex - 1;
+			indices[2] = existingVertexIndex;
+			indices[3] = existingVertexIndex - 1;
+			
+			existingVertexIndex = -1; // previous is now this newly added vertex
+		}
+				
+		// handle old vertex where the previous vertex was new
+		else if (isNewVertex == false)
+		{
+			indices[0] = existingVertexIndex;
+			indices[1] = existingVertexIndex - 1;
+			indices[2] = baseIndex;
+			indices[3] = baseIndex - 1;
+		}
+		
+		//note can't handle two old indices because
+		// isNewVertex == false && existingVertexIndex == -1 // never happens because isNewVertex == false causes a set of existingVertexIndex
+		// return null; //flag that we don't want to add triangles
+		
+		return indices;
 	}
 }
